6563bb10cc9641e3cb6df68701a3ab305b0b7358
aleehjoo <alejandroumila2@gmail.com>
2025-08-29 22:01:40 +0800

    Updates with i18n, compatible with locale switcher. However text-color values in dropdowns are not dynamic.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '../../../i18n/routing';
 import "./styles.css";
+import {setRequestLocale} from "next-intl/server";
 
 export function generateStaticParams() {
     return routing.locales.map((locale) => ({locale}));
@@ -18,6 +19,8 @@ export default async function RootLayout({
     if (!hasLocale(routing.locales, locale)) {
         notFound();
     }
+
+    setRequestLocale(locale);
 
     return (
         <html lang={locale}>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,25 @@
 import { useTranslations } from 'next-intl';
-import { AnimatedThemeToggler } from "@/components/magicui/animated-theme-toggler";
+import { AnimatedThemeToggler } from '@/components/magicui/animated-theme-toggler';
+import { Link } from '../../../i18n/navigation';
+import { setRequestLocale } from 'next-intl/server';
+import { use } from 'react';
+import LocaleSwitcher from "@/app/[locale]/LocaleSwitcher";
 
+const Page = ({params}: { params: Promise<{locale: string}>; }) => {
 
+    const { locale } = use(params);
 
-const Page = () => {
+    setRequestLocale(locale);
+
     const t = useTranslations('home');
 
     return (
-        <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-row gap-20 items-center justify-center">
             <h1 suppressHydrationWarning className="text-xl font-thin">{t('title')}</h1>
             <AnimatedThemeToggler />
+            <Link href="/"></Link>
+
+            <LocaleSwitcher />
         </div>
     )
 }

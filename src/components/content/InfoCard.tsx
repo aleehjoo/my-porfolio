130c8eb5da1c0f2ff2deb90655e9989bcc8419af
aleehjoo <alejandroumila2@gmail.com>
2025-09-03 23:27:43 +0800

    Paddings and stuff

--- src/components/content/InfoCard.tsx
+++ src/components/content/InfoCard.tsx
@@ -1,15 +1,20 @@
 import {useTranslations} from "next-intl";
 
 const InfoCard = () => {
-
-    const t = useTranslations('home');
+    const t = useTranslations("home");
 
     return (
-        <div className="the-shizzle h-55 w-200 rounded-xl border-2">
-            <img/>
-            <p className="mt-8 ml-8 text-3xl font-bold">{t('title')}</p>
-            <p className="ml-8 text-xl color-test-text">{t('content')}</p>
+        <div
+            className="the-shizzle rounded-xl border-2
+                 max-w-[800px] w-full
+                 mx-4 sm:mx-6 md:mx-auto
+                 p-4 sm:p-6"
+        >
+            <img className="hidden" alt="" />
+            <p className="text-2xl sm:text-3xl font-bold">{t("title")}</p>
+            <p className="text-base sm:text-lg mt-2">{t("content")}</p>
         </div>
-    )
-}
-export default InfoCard
+    );
+};
+
+export default InfoCard;

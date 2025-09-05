a2c462e2939135dda787b497b7cd9baaf20e3aaa
aleehjoo <alejandroumila2@gmail.com>
2025-09-05 16:44:48 +0800

    Added more content in info card

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,4 @@
-import { AnimatedThemeToggler } from '@/components/magicui/animated-theme-toggler';
 // import { Link } from '../../../i18n/navigation'; will be used later
-import LocaleSwitcher from "@/components/LocaleSwitcher";
 import InfoCard from '@/components/content/InfoCard';
 import Header from "@/components/Header";
 import TechStack from "@/components/content/TechStack";
@@ -10,8 +8,10 @@ const Page = () => {
     return (
         <div className="flex flex-col gap-16 items-center justify-center">
             <Header />
-            <InfoCard />
-            <TechStack />
+            <div>
+                <InfoCard />
+                <TechStack />
+            </div>
         </div>
     )
 }
--- src/components/content/InfoCard.tsx
+++ src/components/content/InfoCard.tsx
@@ -1,18 +1,43 @@
-import {useTranslations} from "next-intl";
+import { useTranslations } from "next-intl";
+import { MapPin } from "lucide-react";
+import Image from "next/image";
 
 const InfoCard = () => {
-    const t = useTranslations("home");
+    const t = useTranslations("infocard");
 
     return (
         <div
-            className="the-shizzle rounded-xl border-2
-                 max-w-[800px] w-full
-                 mx-4 sm:mx-6 md:mx-auto
-                 p-4 sm:p-6"
+            className="
+        the-shizzle border-2 rounded-xl
+        w-full max-w-2xl mx-4 sm:mx-6 md:mx-auto
+        p-4 sm:p-6
+      "
         >
-            <img className="hidden" alt="" />
-            <p className="text-2xl sm:text-3xl font-bold">{t("title")}</p>
-            <p className="text-base sm:text-lg mt-2">{t("content")}</p>
+            <div className="flex flex-col sm:flex-row gap-4 items-start sm:items-center">
+                <Image
+                    src="/headshot.jpg"
+                    alt="profile"
+                    width={75}
+                    height={75}
+                    className="rounded-full flex-shrink-0"
+                />
+
+                <div className="flex flex-col">
+                    <p className="text-xl sm:text-2xl md:text-3xl font-bold">
+                        {t("name")}
+                    </p>
+                    <div className="flex flex-row gap-1 mt-1 items-center">
+                        <MapPin strokeWidth={1.5} size={20} className="text-gray-500" />
+                        <p className="text-sm sm:text-base md:text-lg text-gray-500">
+                            {t("location")}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <p className="text-sm sm:text-base md:text-lg mt-3 sm:mt-4">
+                {t("description")}
+            </p>
         </div>
     );
 };
